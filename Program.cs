57b572e498580b340f04c2767f52861d6ebb7784
Simon <152021717+SimonR-prog@users.noreply.github.com>
2025-11-28 22:32:34 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+var environmentName = builder.Environment.EnvironmentName;
+
+builder.Configuration.AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true);
+
+var connectionString = builder.Configuration.GetConnectionString("umbracoDbDSN");
+
+builder.CreateUmbracoBuilder()
+    .AddBackOffice()
+    .AddWebsite()
+    .AddComposers()
+    .Build();
+
+WebApplication app = builder.Build();
+
+await app.BootUmbracoAsync();
+
+app.UseHttpsRedirection();
+
+app.UseUmbraco()
+    .WithMiddleware(u =>
+    {
+        u.UseBackOffice();
+        u.UseWebsite();
+    })
+    .WithEndpoints(u =>
+    {
+        u.UseBackOfficeEndpoints();
+        u.UseWebsiteEndpoints();
+    });
+
+await app.RunAsync();
